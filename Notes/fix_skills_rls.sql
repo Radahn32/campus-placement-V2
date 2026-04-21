1e94e0db34d109a034a8c4a38a48ea1fa7812388
Athul R Bangera <athulrb32@gmail.com>
2026-04-22 01:50:01 +0530

    Delete Notes/fix_skills_rls.sql

--- Notes/fix_skills_rls.sql
+++
@@ -1,9 +0,0 @@
--- Fix skills RLS: allow all authenticated users to insert
--- The previous policy only allowed TPO, but students need to add skills too
-DROP POLICY IF EXISTS "skills_add" ON public.skills;
-DROP POLICY IF EXISTS "skills_insert" ON public.skills;
-
-CREATE POLICY "skills_insert_all"
-  ON public.skills FOR INSERT
-  TO authenticated
-  WITH CHECK (true);
